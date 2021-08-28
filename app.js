94b9e5032251ecae7912f2587be3d13e70eeb77a
Savita Verma <svitaverma10@gmail.com>
2021-08-28 10:39:55 +0530

    .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const clickButton = document.querySelector("#click-button");
 
-clickButton.addEventListener("click", function validBillAndCashAmount(clicked) {
-    console.log(clicked);
+clickButton.addEventListener("click", function validBillAndCashAmount() {
+    console.log("clicked");
+    
 })
